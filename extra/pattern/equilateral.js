57c738f49d4b067418aafb468a3ea4c838d88773
KM Sweta Sharma <sweta.sharma@tr011.successivetech.com>
2020-10-16 11:05:46 +0530

    docs:Add diamond.js eqilateral.js

--- extra/pattern/equilateral.js
+++ extra/pattern/equilateral.js
@@ -1,14 +1,14 @@
 function Equilateral(NoOfrow){
-    for(var i=1;i<=NoOfrow;i++){
-        for(var x=NoOfrow-1;x>=i;x--){
+    for(let i=1;i<=NoOfrow;i++){
+        for(let x=NoOfrow-1;x>=i;x--){
             process.stdout.write(" ");
         }
-        for(var j=1;j<=i;j++){
+        for(let j=1;j<=i;j++){
             process.stdout.write("* ");
 
         }
         console.log();
     }
 }
-var y=process.argv[2];
+let y=process.argv[2];
 Equilateral(y)
