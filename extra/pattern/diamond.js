d543894bb2c6ca470358f79e2bd1f09bec82cc49
SwetaSharmaSuccessive <72729965+SwetaSharmaSuccessive@users.noreply.github.com>
2020-10-16 16:05:07 +0530

    Update diamond.js

--- extra/pattern/diamond.js
+++ extra/pattern/diamond.js
@@ -1,35 +1,37 @@
-function printDiamond(n){
-let str;
-for(let i=1; i<=n; i++){
-str = '';
-for(let k=1; k<=n-i; k++)
-{
-str += " ";
-}
-for(let j=1; j<=i; j++)
-{
-str += "* ";
-}
-console.log(str);
-str = "";
-}
 
-for(let i=n; i>0; i--){
-str = '';
-for(let k=n-i; k>0; k--)
-{
-str += " ";
-}
-for(let j=1; j<=i; j++)
-{
-str += "* ";
-}
-console.log(str);
-str = "";
-}
+const f = (rows)=>{ 
+    
+    if((rows>=2) &&( rows<= 10)){
 
 
+        for(let i=1;i<=rows;i++){
+            let sp="";
+            for(let j=rows;j>=i;j--){
+                sp=sp+" ";
+            }
+                
+        for(let k=1;k<=i;k++){
+            sp=sp+"* ";
+        }
+        console.log(sp); 
+        }
+            
+    for(let i=rows;i>=1;i--){
+        let sp="";
+        for(let j=rows;j>=i;j--){
+            sp=sp+" ";
+                    
+        }
+        for(let k=i;k>=1;k--){
+            sp=sp+"* ";
+        }
+            
+        console.log(sp); 
+        
+    }
+            
+    }
 }
-
-n = process.argv[2];
-printDiamond(n);
+ let y=process.argv[2];
+     f(y);  
+    
