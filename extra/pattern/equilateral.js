0bae14c9b0b1553d1f57c322877bf32214ac26a5
SwetaSharmaSuccessive <72729965+SwetaSharmaSuccessive@users.noreply.github.com>
2020-10-16 18:08:04 +0530

    Update equilateral.js

--- extra/pattern/equilateral.js
+++ extra/pattern/equilateral.js
@@ -1,5 +1,6 @@
-let A=(NoOfrow)=>{
-    if((NoOfrow>=2) &&(NoOfrow<= 10)){
+let EquilateralTriangle=(NoOfrow)=>{
+
+    if((NoOfrow>=2) &&(NoOfrow<= 10)){ 
     for(let i=1;i<=NoOfrow;i++){
         for(let x=NoOfrow-1;x>=i;x--){
             process.stdout.write(" ");
@@ -10,9 +11,10 @@ let A=(NoOfrow)=>{
         }
         console.log();
     }
-    }
-    else{
-        console.log('error: something is wrong');
+}
+else{
+    console.log("error: Something is wrong");
+}
 }
 let y=process.argv[2];
-A(y);
+EquilateralTriangle(y)
