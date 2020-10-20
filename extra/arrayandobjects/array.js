b2429fdfd4cc77710764281c40b7ad0cf6e8a0fd
SwetaSharmaSuccessive <sweta.sharma@tr011.successivetech.com>
2020-10-20 18:57:51 +0530

    Modify: Array and object file

--- extra/arrayandobjects/array.js
+++ extra/arrayandobjects/array.js
@@ -3,61 +3,61 @@ let fast_food=['samosa','burger','pizza']
 
 // Array length
 
-console.log(fast_food.length);
+console.log('lenth of array is: ', fast_food.length);
 
 //Access an Array item using the index position
 
 let indian_food=fast_food[0] ;   // first element
-console.log(indian_food);
+console.log('first element of the array: ', indian_food);
 let  italian_food=fast_food[fast_food.length-1] ;    // last element 
-console.log(italian_food);
+console.log('last element of the array: ', italian_food);
 
 let new_length=fast_food.push('DOSA');     //push()
-console.log(new_length);
+console.log('new length of the array after pushing new element in the array: ', new_length);
 let old_lenght=fast_food.pop('DOSA');       //pop()
-console.log(old_lenght)
+console.log('new length of the array after poping element: ', old_lenght)
 
 let first=fast_food.shift();                // remove samosa from the front
-console.log(first)
+console.log('removed element from the first of the array: ', first)
 
 let newLength1 = fast_food.unshift(' maggie')   // maggie add to the front
-console.log(newLength1)
+console.log('after added new element to the front of array new length: ', newLength1)
 
 
 
 let Copy = fast_food.slice()                   // Copy an Array
-console.log(Copy);
+console.log('copy of array: ', Copy);
 
-let pos = fast_food.indexOf('maggie')       
-console.log(pos)
+let pos = fast_food.indexOf('burger')       
+console.log('position of burger in array:  ', pos)
 let removedItem = fast_food.splice(pos, 1)
-console.log(fast_food)
+console.log('removed 1st item in array: ', fast_food)
 
 // Array.from()
 
-console.log(Array.from('pizza'));
+console.log('Array.from()--->', Array.from('pizza'));
 
 //Array.isArray() 
 
 let x=Array.isArray('fast_food')
-console.log(x);
+console.log('Array.isArray() ---->', x);
 
 //ArrayOf
 
 let veg_food=Array.of('panner', 'chhole_bhtaure', 'pav_bhaji');
 console.log(`veg food --> ${veg_food}`);                       
-console.log(veg_food)
+console.log('veg food in form of array: ', veg_food)
 
 //Array.concat()
 
 let food=fast_food.concat(veg_food);
-console.log(food)
+console.log('Array.concat()--->', food)
 
 //Array.reverse()
 
 
 const reversed = fast_food.reverse();
-console.log('reversed:', reversed);
+console.log('reversed array:', reversed);
 
 // Array.copyWithin()
 
