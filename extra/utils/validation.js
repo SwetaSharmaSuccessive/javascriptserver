01cc5786489b150bd351c46375300b32c5353b6f
SwetaSharmaSuccessive <sweta.sharma@tr011.successivetech.com>
2020-10-22 17:09:15 +0530

    modified files

--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -1,67 +1,39 @@
-let users=[
+
+const users = [
     {
-    traineeEmail: 'trainee1@successive.tech',
-    reviewerEmail: 'reviewer1@successive.tech',
+    traineeEmail: 'trainee.S@successive.tech',
+    reviewerEmail: 'avinash.thube@successive.tech'
     },
+    
     {
-    traineeEmail: 'xyz@gmail.com',
-    reviewerEmail: 'abc@gmail.com',
+    traineeEmail: 'trainee2.A@live.in' ,
+    reviewerEmail: 'reviewer2.B@successive.tech'
     },
+    
     {
-    traineeEmail: 'ftc@successive.tech',
-    reviewerEmail: 'ftcre@successive.tech',
-    },
-    {
-    traineeEmail: 'trainee2@successive.tech',
-    reviewerEmail: 'reviewer2@successive.tech',
-    },
-    {
-    traineeEmail: 'trainee3@successive.tech',
-    reviewerEmail: 'reviewer3@successive.tech',
-    }
-    ]
-    
-    function validateEmail(email){
-    let reg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    let domain=email.split('@')[1];
-    if(reg.test(email) && (domain=='successive.tech')){
-        return true;
-    }
-    else{
-        return false;
-    }
+    traineeEmail: 'trainee3.C@gmail.com',
+    reviewerEmail: 'reviewer3.D@successive.tech'
     }
     
+    ];
     
-    function validateUsers(users){
-    vuser=[];
-    iuser=[];
-    let icount=0;
-    let vcount=0;
-    users.forEach(({traineeEmail,reviewerEmail}) => {
-    if(validateEmail(traineeEmail)&&validateEmail(reviewerEmail))
-    {
-    icount+=1;
-    vuser.push({traineeEmail,reviewerEmail});
-    return icount;
+    
+const validateEmail = (email) => {
+    const regex = /\w+[.]\w+@successive.tech$/i;
+    return regex.test(email)
+}
+    
+    
+const validateUsers = (user) =>{
+    const validUsers = [];
+    const invalidUsers = [];
+    user.forEach(({traineeEmail, reviewerEmail}) => {
+    validateEmail(traineeEmail) ? validUsers.push(traineeEmail) : invalidUsers.push(traineeEmail)
+    validateEmail(reviewerEmail) ? validUsers.push(reviewerEmail) : invalidUsers.push(reviewerEmail)
     }
-    else
-    {
-    vcount+=1;
-    iuser.push({traineeEmail,reviewerEmail});
-    return vcount;
-    }
-    });
+    )
+    console.log(`There are ${validUsers.length} valid users:`, validUsers)
+    console.log(`There are ${invalidUsers.length} invalid users:`, invalidUsers)
     
-    
-    console.log("Number of valid users:", icount);
-    console.log("Number of Invalid users: ", vcount);
-    console.log();
-    console.log("valid users: ", vuser);
-    console.log("Invalid users: ", iuser);
-    console.log();
-    
-    
-    }
-    
-    validateUsers(users);
+}
+    validateUsers(users)
