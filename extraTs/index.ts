9cb1768e13a077a2c833ba4d314398ef4b74965a
SwetaSharmaSuccessive <sweta.sharma@successive.tech>
2020-10-26 14:14:05 +0530

    Add files

---
+++ extraTs/index.ts
@@ -0,0 +1,10 @@
+import  {DiamondPattern, EquilateralTriangle} from './pattern';
+import { hasPermissions,validateUsers} from './utils';
+import { users} from './constant';
+
+console.log('print a diamond pattern with rows 5');
+DiamondPattern(5);
+console.log("print a equilateral triangle with rows 5");
+EquilateralTriangle(5);
+hasPermissions("getUsers2","HR dept", "read");
+validateUsers(users);
---
+++ extraTs/utils/validation.ts
@@ -0,0 +1,17 @@
+import validateEmail from './helpers';
+        
+const validateUsers = (user: string) =>{
+    const validUsers = [];
+    const invalidUsers = [];
+    user.forEach(({traineeEmail, reviewerEmail}) => {
+        validateEmail(traineeEmail) ? validUsers.push(traineeEmail) : invalidUsers.push(traineeEmail)
+        validateEmail(reviewerEmail) ? validUsers.push(reviewerEmail) : invalidUsers.push(reviewerEmail)
+    }
+    )
+    console.log(`There are ${validUsers.length} valid users:`, validUsers)
+    console.log(`There are ${invalidUsers.length} invalid users:`, invalidUsers)
+    
+}
+
+export default validateUsers;
+    
