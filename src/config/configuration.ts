453907a7c9c9074afa9b29066a7e1980293f1e7b
SwetaSharmaSuccessive <sweta.sharma@successive.tech>
2020-10-27 01:39:57 +0530

    Add files

---
+++ src/config/configuration.ts
@@ -0,0 +1,7 @@
+
+import * as dotenv from 'dotenv';
+const envVars = dotenv.config();
+const config = envVars.parsed;
+export default config;
+console.log('config is', config);
+Object.freeze(config);
