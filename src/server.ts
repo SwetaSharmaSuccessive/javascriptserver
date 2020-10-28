dd4eaca839a25910991c35dd073c1937b76fc4f6
SwetaSharmaSuccessive <sweta.sharma@successive.tech>
2020-10-28 19:01:51 +0530

    HTTP status code and handler

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import { IConfig } from './config/IConfig';
+import * as bodyparser from 'body-parser';
+import { notFoundHandler, errorHandler } from './libs/routes';
 class Server {
     private app: any;
     constructor(private config: IConfig) {
@@ -7,13 +9,21 @@ class Server {
     }
     bootstrap() {
         this.SetupRoutes();
+        this.initBodyParser();
         return this;
     }
     SetupRoutes() {
-        this.app.get('/health-check', (req, res, next) => {
+
+        this.app.use('/health-check', (req, res, next) => {
+            console.log('inside second middleware');
             res.send('I am Ok');
         });
-        return this;
+        this.app.use(notFoundHandler);
+
+        this.app.use(errorHandler);
+    }
+    public initBodyParser() {
+        this.app.use(bodyparser.json({ type: 'application/*+json' }));
     }
 
     run() {
